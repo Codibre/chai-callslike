4a99baf660438cbe9ba802fd25d93f464ae80b36
Thiago Santos <thiago.santos@maxmilhas.com.br>
2021-01-27 22:19:45 -0300

    fixing recursive object comparing

--- test/like.spec.ts
+++ test/like.spec.ts
@@ -51,4 +51,26 @@ describe('like', () => {
   it('should pass properly comparing a null value with null', () => {
     expect(null).to.have.like(null);
   });
+
+  it('shoud work fine comparing a recursive object to itself', () => {
+    const test: any = {};
+    test.test = test;
+
+    expect(test).to.be.like(test);
+  });
+
+  it('shoud throw and treated error when comparing a recursive object with something different', () => {
+    let error: any;
+
+    try {
+      const test: any = {};
+      test.test = test;
+
+      expect(test).to.be.like({ test: 'something else' });
+    } catch (err) {
+      error = err;
+    }
+
+    expect(error.message).to.match(/Value's not like the expected.+/);
+  });
 });
